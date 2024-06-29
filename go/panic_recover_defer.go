562cbd98c7c51f76b10b2456af1bafb8005e6c1e
Asef264 <xhmoazedi@gmail.com>
2024-06-29 13:28:13 +0330

    add some another tip

--- go/panic_recover_defer.go
+++ go/panic_recover_defer.go
@@ -14,4 +14,7 @@ func main() {
 the defer insures some function executs on any situations ! it executs befor any other functions or returnings or paincs or errors
 recover in other hand records the cause of the panic
 and panic clearly stops the execution when ever it comes along
+A panic generally indicates a programmer error (for
+example attempting to access an index of an array
+that's out of bounds, forgetting to initialize a map, etc.)
 */
